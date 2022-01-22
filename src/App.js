4ef35890dfdaa66e255e87c9dd16e842af1612ea
Henrike Wiemker <65461017+HenrikeW@users.noreply.github.com>
2022-01-22 13:59:37 +0100

    functionality to show a random recipe

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {React, useState} from 'react';
 import './App.css';
+import {Recipe} from "./components/Recipe.js"
+import {recipes} from "./constants/recipes.js"
 
 const App = () => {
+  const [currentRecipe, setCurrentRecipe] = useState("");
+  const allRecipes = recipes;
+
+  const getRandomRecipe = () => {
+    const chosenRecipe = allRecipes[Math.floor(Math.random() * allRecipes.length)].name
+    setCurrentRecipe(chosenRecipe)
+    return
+  }
   return (
-    <div>All empty</div>
+    <>
+    <h1>Hitta dagens mat</h1>
+    <Recipe recipe = {currentRecipe} />
+    <button onClick={() => getRandomRecipe()}>Ge mig inspo</button>
+    </>
   );
 }
 
