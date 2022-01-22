45150cbc4b1b5f4c6e7a66d5bfed9b0971746292
Henrike Wiemker <65461017+HenrikeW@users.noreply.github.com>
2022-01-22 16:32:11 +0100

    add more recipes

--- src/constants/recipes.js
+++ src/constants/recipes.js
@@ -10,5 +10,81 @@ export const recipes = [
   {
     id: 3,
     name: "Bönlasagn"
+  },
+  {
+    id: 4,
+    name: "Pannkakor"
+  },
+  {
+    id: 5,
+    name: "Pasta med köttfärssås"
+  },
+  {
+    id: 6,
+    name: "Svamprisotto"
+  },
+  {
+    id: 7,
+    name: "Wok med nudlar"
+  },
+  {
+    id: 8,
+    name: "Kålpudding"
+  },
+  {
+    id: 10,
+    name: "Laxgryta med vitkål och rödbetor"
+  },
+  {
+    id: 11,
+    name: "Ungsbakad feta med tomater och citron"
+  },
+  {
+    id: 12,
+    name: "Grön ärtsoppa"
+  },
+  {
+    id: 13,
+    name: "Morrotsgryta"
+  },
+  {
+    id: 14,
+    name: "Fiskpinnar med potatismos och spenat"
+  },
+  {
+    id: 15,
+    name: "Fish and chips med mosade ärtor"
+  },
+  {
+    id: 16,
+    name: "Paj med pumpa och purjolök"
+  },
+  {
+    id: 17,
+    name: "Gryta med röda linser och russin"
+  },
+  {
+    id: 18,
+    name: "Dhal med ris eller naanbröd"
+  },
+  {
+    id: 19,
+    name: "Hembakad pizza"
+  },
+  {
+    id: 20,
+    name: "Fryspizza"
+  },
+  {
+    id: 21,
+    name: "Lökpizza"
+  },
+  {
+    id: 22,
+    name: "Ratattouille med saffran"
+  },
+  {
+    id: 23,
+    name: "Vegoschnitzel med potatis och sallad"
   }
 ]
