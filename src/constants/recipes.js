3cc1298aab4eacc3b0b32c105012fb158d0a5f84
Henrike Wiemker <65461017+HenrikeW@users.noreply.github.com>
2022-01-22 13:58:29 +0100

    create list of recipes

---
+++ src/constants/recipes.js
@@ -0,0 +1,14 @@
+export const recipes = [
+  {
+    id: 1,
+    name: "Purjolökspizza"
+  },
+  {
+    id: 2,
+    name: "Risotto med päron och blåmögelost"
+  },
+  {
+    id: 3,
+    name: "Bönlasagn"
+  }
+]
